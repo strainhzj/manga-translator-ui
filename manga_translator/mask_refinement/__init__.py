3433813ef8964a4d4902a9ed59a54ca03c2112f3
hgmzhn <hgmzhn@gmail.com>
2026-01-12 22:58:44 +0800

    移除 mask_refinement 中的 ignore_bubble 过滤逻辑
    - ignore_bubble 只在 OCR 阶段过滤，不在蒙版精炼阶段过滤
    - 避免重复过滤导致的不一致问题

--- manga_translator/mask_refinement/__init__.py
+++ manga_translator/mask_refinement/__init__.py
@@ -4,7 +4,6 @@ import numpy as np
 
 from .text_mask_utils import complete_mask_fill, complete_mask
 from ..utils import TextBlock, Quadrilateral
-from ..utils.bubble import is_ignore
 
 async def dispatch(text_regions: List[TextBlock], raw_image: np.ndarray, raw_mask: np.ndarray, method: str = 'fit_text', dilation_offset: int = 0, ignore_bubble: int = 0, verbose: bool = False,kernel_size:int=3) -> np.ndarray:
     # Larger sized mask images will probably have crisper and thinner mask segments due to being able to fit the text pixels better
@@ -27,21 +26,5 @@ async def dispatch(text_regions: List[TextBlock], raw_image: np.ndarray, raw_mas
     else:
         final_mask = cv2.resize(final_mask, (raw_image.shape[1], raw_image.shape[0]), interpolation = cv2.INTER_LINEAR)
         final_mask[final_mask > 0] = 255
-
-    if ignore_bubble <= 0 or ignore_bubble > 1:
-        return final_mask
-
-    # 先检测并移除非气泡区域，再进行膨胀
-    # border
-    contours, _ = cv2.findContours(final_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        temp_mask = np.zeros_like(final_mask)
-        # rect min
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(temp_mask, (x, y), (x + w, y + h), 255, -1)
-        # get textblock
-        textblock=cv2.bitwise_and(raw_image, raw_image, mask=temp_mask)
-        if is_ignore(textblock, ignore_bubble):
-            cv2.drawContours(final_mask, [cnt], -1, 0, -1)
 
     return final_mask
